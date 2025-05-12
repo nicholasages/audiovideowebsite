ab10a416a3b4a8cbfa55fa440873f844bd2493b3
nicholasages <nicholasy34@nycstudents.net>
2025-05-12 12:15:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,41 @@ function playPause() {
     playing = !playing;
 }
 
+const video2 = document.getElementById('video2');
+const audio2 = document.getElementById('audio2');
+document.getElementById('start').addEventListener('click', () => {
+    playPause2();
+});
+
+let playing2 = false;
+
+function playPause2() {
+    if (playing) {
+        video2.pause();
+        audio2.pause();
+    } else {
+        video2.play();
+        audio2.play();
+    }
+    playing2 = !playing2;
+}
+
+const video3 = document.getElementById('video3');
+const audio3 = document.getElementById('audio3');
+document.getElementById('start').addEventListener('click', () => {
+    playPause3();
+});
+
+let playing3 = false;
+
+function playPause3() {
+    if (playing) {
+        video3.pause();
+        audio3.pause();
+    } else {
+        video3.play();
+        audio3.play();
+    }
+    playing3 = !playing3;
+}
+
